e7f1e9d76d00d59d18a8df62a4efbd559b79dd57
ghoti <122617510+ghoti-GUI@users.noreply.github.com>
2023-11-19 16:50:45 +0100

    Add files via upload

--- loadData.py
+++ loadData.py
@@ -1,98 +1,185 @@
 import torch
 import pandas as pd
 import numpy as np
-from torch.utils.data import Dataset, DataLoader
+from torch.utils.data import Dataset, DataLoader, random_split
 from torchvision import transforms
 from PIL import Image
 from sklearn.model_selection import KFold
+import os
 
-dataindex = range(10000)
 transform = transforms.Compose([
     transforms.Resize((224, 224)),
     transforms.ToTensor(),
     transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
 ])
 
+transform_train = transforms.Compose([
+    transforms.Resize((224, 224)),
+    transforms.ToTensor(),
+    transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]), 
+    transforms.RandomRotation(10),  # 随机旋转
+    # transforms.ColorJitter(brightness=0.2, contrast=0.2),  # 随机调整亮度，对比度
+])
+
 train_loader = []
-test_loader = []
+valid_loader = []
 n_splits = 8
-
-#建立自己的dataset
-class CreateDatasetFromImages(Dataset):
-    def __init__(self, csv_info, file_path, data_index = dataindex, resize_height=224, resize_width=224, transform = transform):
-
-        self.resize_height = resize_height
-        self.resize_width = resize_width
- 
-        # csv_path = "./isic-2020-resized/train-labels.csv"
-        # train_path = "./isic-2020-resized/train-resized"
-        self.file_path = file_path
-        self.transform = transform
- 
-        self.data_info = csv_info 
-        # self.data_info = pd.read_csv(csv_path)
-        self.data_info = self.data_info.iloc[data_index]
-
- 
-    def __getitem__(self, index):
-        label = torch.tensor(int(self.data_info.iloc[index, 1]))
-        img_as_img = Image.open(self.file_path + self.data_info.iloc[index, 0] + ".jpg")
-        img_as_img = self.transform(img_as_img)
-        return img_as_img, label  #返回每一个index对应的图片数据和对应的label
-        
-    def __len__(self):
-        return len(self.data_info)-1
-    
-
-
+n_splits_1 = 8
+batch_size = 10
+size_data = 0
 
 csv_path = "./isic-2020-resized/train-labels.csv"
 data_path = "./isic-2020-resized/train-resized/"
+test_path = "./isic-2020-resized/test-resized/"
+test_csv_path = "./isic-2020-resized/test.csv"
 
-csv_info = pd.read_csv(csv_path)
-size_data = len(csv_info)
-# print(size_data)
+file_name = 'test_data.txt'
+try:
+    os.remove(file_name)
+    print(f'{file_name} delete success。')
+except FileNotFoundError:
+    print(f'{file_name} do not exist')
+except Exception as e:
+    print(f'delete {file_name} error: {e}')
 
-# add more class 1 to the data
-nbr_copy = 10
+with open(file_name, 'a') as file:
+    csv_info = pd.read_csv(csv_path)
+    size_data = len(csv_info)
+    file.write(f"data size init = {size_data}\n")
+    print(f"data size init = {size_data}")
 
-selected_rows = csv_info[csv_info.iloc[:, 1] == 1]
+    #建立自己的dataset
+    # data_index 原本用于区分trans data和valid data
+    class CreateDatasetFromImages(Dataset):
+        def __init__(self, csv_info, file_path, data_index = range(size_data), resize_height=224, resize_width=224, transform = transform):
 
-# 复制并添加这些行到DataFrame
-for _ in range(nbr_copy): 
-    csv_info = pd.concat([csv_info, selected_rows.copy()], ignore_index=True)
-
-size_data += nbr_copy*len(selected_rows)
-# print(size_data)
-
-
-
-# to seperate train_dataset and test_dataset
-data_index = [None]*size_data
-
-kf = KFold(n_splits=n_splits, shuffle=True)
-
-for i, (train_index, test_index) in enumerate(kf.split(data_index)):
-    # print(f"Fold {i}:")
-    # print(f"  Train: index={train_index}")
-    # print(f"  Test:  index={test_index}")
+            self.resize_height = resize_height
+            self.resize_width = resize_width
     
-    TrainDataset = CreateDatasetFromImages(csv_info, data_path, train_index)
-    TestDataset = CreateDatasetFromImages(csv_info, data_path, test_index)
+            # csv_path = "./isic-2020-resized/train-labels.csv"
+            # train_path = "./isic-2020-resized/train-resized"
+            self.file_path = file_path
+            self.transform = transform
+    
+            self.data_info = csv_info 
+            # self.data_info = pd.read_csv(csv_path)
+            # self.data_info = self.data_info.iloc[data_index]
+
+    
+        def __getitem__(self, index):
+            label = torch.tensor(int(self.data_info.iloc[index, 1]))
+            img_as_img = Image.open(self.file_path + self.data_info.iloc[index, 0] + ".jpg")
+            img_as_img = self.transform(img_as_img)
+            return (img_as_img, label)  #返回每一个index对应的图片数据和对应的label
+            
+        def __len__(self):
+            return len(self.data_info)
+
+    # 处理获取的csv文件，提取出label 1 和label 0
+
+    # # add more class 1 to the data
+    # nbr_copy = 10
+    # selected_rows = csv_info[csv_info.iloc[:, 1] == 1]
+    # # 复制并添加这些行到DataFrame
+    # for _ in range(nbr_copy): 
+    #     csv_info = pd.concat([csv_info, selected_rows.copy()], ignore_index=True)
+
+    # size_data += nbr_copy*len(selected_rows)
+
+    # find label 1 and diviser en 1:7
+    img_1 = csv_info[csv_info.iloc[:, 1] == 1]
+    nbr_1 = len(img_1)
+    split1 = nbr_1//n_splits_1
+    img_1_valid = img_1.sample(n=split1, random_state=2) 
+    img_1_train = img_1.drop(img_1_valid.index)
+    file.write(f"len_img_1_train = {len(img_1_train)}, len_img_1_valid = {len(img_1_valid)}\n")
+    print(f"len_img_1_train = {len(img_1_train)}, len_img_1_valid = {len(img_1_valid)}")
+
+    # extrait tous les 1 dans les img
+    img_0 = csv_info[csv_info.iloc[:, 1] == 0]
+    size_data = len(img_0)
+    file.write(f"data size after extrait = {size_data}\n")
+    print(f"data size after extrait = {size_data}")
+
+    # 将csv文件分成1:7
+    split0 = size_data//n_splits
+    img_valid = img_0.sample(n=split0, random_state=1) 
+    img_train = img_0.drop(img_valid.index)
+    file.write(f"size train no 1 = {len(img_train)}, size valid no 1 = {len(img_valid)}\n")
+    print(f"size train no 1 = {len(img_train)}, size valid no 1 = {len(img_valid)}")
+
+    # 重置索引
+    img_train = img_train.reset_index(drop=True)
+    img_valid = img_valid.reset_index(drop=True)
+
+    # copy 10 times of img_1_train to img_train
+    n_copytrain = 1
+    copy_img = [img_1_train.copy() for _ in range(n_copytrain)]
+    add_img = pd.concat(copy_img, ignore_index=True)
+    img_train = pd.concat([img_train, add_img], ignore_index=True)
+
+    # copy 1 times of img_1_valid to img_valid
+    n_copyvalid = 1
+    copy_img = [img_1_valid.copy() for _ in range(n_copyvalid)]
+    add_img = pd.concat(copy_img, ignore_index=True)
+    img_valid = pd.concat([img_valid, add_img], ignore_index=True)
+    file.write(f"size train with 1 = {len(img_train)}, size valid with 1 = {len(img_valid)}\n")
+    print(f"size train with 1 = {len(img_train)}, size valid with 1 = {len(img_valid)}")
+
+    # TrainDataset = CreateDatasetFromImages(img_train, data_path, transform=transform_train)
+    TrainDataset = CreateDatasetFromImages(img_train, data_path)
+    # ValidDataset = CreateDatasetFromImages(img_valid, data_path, transform=transform_train)
+    ValidDataset = CreateDatasetFromImages(img_valid, data_path)
+
+
+    file.write(f"size TrainDataset = {len(TrainDataset)}, size ValidDataset = {len(ValidDataset)}\n")
+    print(f"size TrainDataset = {len(TrainDataset)}, size ValidDataset = {len(ValidDataset)}")
+
+
     train_loader.append(
         torch.utils.data.DataLoader(
             dataset=TrainDataset,
-            batch_size=32, 
+            batch_size=batch_size, 
             shuffle=True,
         ))
-    test_loader.append(
+
+    valid_loader.append(
         torch.utils.data.DataLoader(
-            dataset=TestDataset,
-            batch_size=32, 
+            dataset=ValidDataset,
+            batch_size=batch_size, 
             shuffle=False,
         ))
 
-    # print(f"length_train = {len(TrainDataset)}")
-    # print(f"length_test = {len(TestDataset)}")
+
+    class CreateTestDataset(Dataset):
+        def __init__(self, csv_info, file_path, data_index = range(10982), resize_height=224, resize_width=224, transform = transform):
+
+            self.resize_height = resize_height
+            self.resize_width = resize_width
+
+            self.file_path = file_path
+            self.transform = transform
+            
+            self.data_info = csv_info 
+            # self.data_info = pd.read_csv(csv_path)
+            self.data_info = self.data_info.iloc[data_index]
+
+
+        def __getitem__(self, index):
+            img_name = self.data_info.iloc[index, 0]
+            img_as_img = Image.open(self.file_path + img_name + ".jpg")
+            img_as_img = self.transform(img_as_img)
+            return img_as_img, img_name
+            
+        def __len__(self):
+            return len(self.data_info)
+    
+    test_csv = pd.read_csv(test_csv_path)
+    TestDataset = CreateTestDataset(test_csv, test_path)
+        
+    test_loader = torch.utils.data.DataLoader(
+            dataset=TestDataset,
+            shuffle=False,
+        )
 
 
--- train_test.py
+++ train_test.py
@@ -3,34 +3,46 @@ import torch
 import torch.optim as optim
 import torch.nn as nn
 import numpy as np
+import os
+import csv
+# import matplotlib.pyplot as plt
 
-from loadData import train_loader, test_loader, n_splits
-from network import resnet50
+from loadData import train_loader, valid_loader, n_splits, file_name, test_loader
+# from loadData_copy import train_loader, valid_loader, n_splits, file_name, test_loader
+from network import resnet50, ResNet
+from ZHAO.net_z import NeuralNetwork
 
+
+batch_nbr = 500
+epochs = 50
+lr = 0.001
+momentum = 0.9
 
 # set net model
 device = torch.device("cuda:0" if torch.cuda.is_available() else "CPU")
 
 net = resnet50()
+# net = ResNet()
+# net = NeuralNetwork()
 net.to(device)
 
 # nn.utils.clip_grad_norm_(net.parameters(), max_norm = 1.0)
 loss_func = nn.CrossEntropyLoss()  # loss function
-optimizer = optim.SGD(net.parameters(), lr=0.00001, momentum=0.9) # optimizer
+optimizer = optim.SGD(net.parameters(), lr=lr, momentum=momentum) # optimizer
+# optimizer = optim.Adagrad(net.parameters(), lr=lr)
+# optimizer = optim.Adam(net.parameters(), lr=0.001, betas=(0.9, 0.999))
 
-batch_size = 100
-
-def validate(test_loader, net, loss_func):
-    print("--test: ", end = "")
+def validate(valid_loader, net, loss_func, file):
+    file.write(" --> valid: ")
+    print(" --> valid: ", end = "")
     correct_0 = 0
     correct_1 = 0
-    # total = 0
     total_0 = 0
     total_1 = 0
     total_loss = 0.0
     with torch.no_grad():
         net.eval()  # evaluation mode
-        for images, labels in test_loader:
+        for images, labels in valid_loader:
             images, labels = images.to(device), labels.to(device) 
 
             # print(f"\nlabels = {labels}")
@@ -46,46 +58,100 @@ def validate(test_loader, net, loss_func):
             total_1 += (labels == 1).sum().item()
             correct_0 += ((labels == 0) & (predicted == 0)).sum().item()
             correct_1 += ((labels == 1) & (predicted == 1)).sum().item()
-    
 
-    print('Average loss: %.3f' % (total_loss / len(train_loader)), end = '\t')
-    print('Accuracy class[0]: %.3f %% [%5f/%f]' % (100 * correct_0 / total_0, correct_0, total_0), end = '\t')
-    print('Accuracy class[1]: %.3f %% [%5f/%f]' % (100 * correct_1 / total_1, correct_1, total_1))
+    # 写入内容到文件
+    file.write('Average loss: %4.3f    ' % (total_loss / len(train_loader)))
+    file.write('Accuracy class[0]: %4.3f %% [%4d/%d]    ' % (100 * correct_0 / total_0, correct_0, total_0))
+    file.write('Accuracy class[1]: %4.3f %% [%4d/%d]\n' % (100 * correct_1 / total_1, correct_1, total_1))
+
+    print('Average loss: %.3f    ' % (total_loss / len(train_loader)), end = '')
+    print('Accuracy class[0]: %4.3f %% [%4d/%d]    ' % (100 * correct_0 / total_0, correct_0, total_0), end = '')
+    print('Accuracy class[1]: %4.3f %% [%4d/%d]    ' % (100 * correct_1 / total_1, correct_1, total_1))
+
+def test(test_loader, net):
+    estimated_test_csv = "./isic-2020-resized/test_estimated.csv"
+    print("---Test Start---")
+    img_label_ls = []
+    with torch.no_grad():
+        net.eval()  # evaluation mode
+        for images, img_name in test_loader:
+            # print(f"image_name = {img_name}")
+            images = images.to(device)
+
+            outputs = net(images) # outputs = [[a, b]...], a 表示class 0的概率，b表示1的概率
+
+            _, predicted = torch.max(outputs, 1)
+            # print(f"estimaed labels = {predicted}")
+            img_label = [img_name[0], predicted.item()]
+            # print(f"img_label = {img_label}")
+
+            img_label_ls.append(img_label)
+
+    # print(img_label_ls)
+    with open(estimated_test_csv, 'w', newline='') as csvfile:
+        csv_writer = csv.writer(csvfile)
+        csv_writer.writerow(['image_name', 'target']) 
+        for img_label in img_label_ls:
+            csv_writer.writerow(img_label)
+
+    print("test fini !!!")
+
+
 
 if __name__ == '__main__':
-    print("Start Training...\n")
-    for i in range(1):
-        print(f"------dataset: {i}------")
-        train_data = train_loader[i]
-        test_data = test_loader[i]
-        for epoch in range(3): 
-            print('-- Epoch %d --'%(epoch+1))
-            running_loss = 0.0
-            for batch, data in enumerate(train_data, 0):
-                # data: list[inputs,labels]
-                inputs, labels = data
-                inputs, labels = inputs.to(device), labels.to(device)
-                # print(labels.size())
-                # print(inputs,labels)
 
-                outputs = net(inputs)
-                # print(outputs.size())
-                loss = loss_func(outputs, labels)
+    with open(file_name, 'a') as file:
+        file.write(f"loss function: {loss_func}\n")
+        file.write(f"optimizer: {optimizer}\n")
+        file.write(f"learning rate = {lr}, momentum = {momentum}\n")
+        file.write(f"number of epochs = {epochs}\n")
+        file.write(f"return loss after each {batch_nbr} batchs\n")
+        file.write("\nStart Training...\n")
 
-                running_loss += loss.item()
-                if batch % batch_size == batch_size-1: 
-                    print('Batch %4d, loss: %.3f' % (batch + 1, running_loss / batch_size), end = "\t")
-                    validate(test_data, net, loss_func)
-                    running_loss = 0.0
+        print(f"loss function: {loss_func}")
+        print(f"optimizer: {optimizer}")
+        print(f"learning rate = {lr}, momentum = {momentum}")
+        print(f"number of epochs = {epochs}")
+        print(f"return loss after each {batch_nbr} batchs")
+        
+        print("\nStart Training...")
+        for i in range(1):
+            file.write(f"------dataset: {i}------\n")
+            print(f"------dataset: {i}------")
+            train_data = train_loader[i]
+            test_data = valid_loader[i]
+            for epoch in range(epochs): 
+                file.write('-- Epoch %d --\n'%(epoch+1))
+                print('-- Epoch %d --'%(epoch+1))
+                running_loss = 0.0
+                for batch, data in enumerate(train_data, 0):
+                    inputs, labels = data
+                    inputs, labels = inputs.to(device), labels.to(device)
 
-                nn.utils.clip_grad_norm_(net.parameters(), max_norm = 1.0)
-                optimizer.zero_grad()
-                loss.backward()
-                optimizer.step()
+                    outputs = net(inputs)
+                    loss = loss_func(outputs, labels)
 
-        # validate(test_data, net, loss_func)
+                    running_loss += loss.item()
+                    if batch % batch_nbr == batch_nbr-1: 
+                        file.write('Batch %4d, loss: %.3f    ' % (batch + 1, running_loss / batch_nbr))
+                        print('Batch %4d, loss: %.3f' % (batch + 1, running_loss / batch_nbr), end = "\t")
+                        validate(test_data, net, loss_func, file)
+                        running_loss = 0.0
 
-    print('\nFinished Training\n\n')
+                    nn.utils.clip_grad_norm_(net.parameters(), max_norm = 1.0)
+                    optimizer.zero_grad()
+                    loss.backward()
+                    optimizer.step()
+
+                # validate(test_data, net, loss_func)
+
+        file.write('\nFinished Training\n\n\n')
+        print('\nFinished Training\n\n')
+
+        file.write('\nFinished Training\n\n\n')
+        print('\nFinished Training\n\n')
+        test(test_loader, net)
+
 
 
 
